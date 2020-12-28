f13ff1c9c629dc64807aabc0348e6c2242af4a58
Anders <anders.sobakken@gmail.com>
2020-12-28 19:08:14 +0100

    Initial commit

---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render() {
+      return(
+        <header>
+            isthislive?
+            maybenot but not sure
+        </header>  
+      );
+    }
+  }
+
+  export default Header;
